b39c2270b0b731e853613c56b1d02edd2ffe5f48
Yarin Kessler <ykessler@appgrinders.com>
2021-04-04 18:40:49 +0000

    Change .replit file

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
 console.log('foo');
+
+jQuery(function () {
+  console.log('hi guy');
+  //console.log('hello');
+  jQuery('.test_button').click(function(e){
+    console.log('BUTTON CLICK 1');
+  });
+  jQuery(document).on('click','.test_button',function(e){
+    console.log('BUTTON CLICK 2');
+  });
+  jQuery(document).on('mousedown','.test_button',function(e){
+    console.log('BUTTON DOWN');
+  });
+  
+});
